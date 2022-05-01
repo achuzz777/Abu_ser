3ba2364b592eca9b9296ce3183f619c593c66b20
achuzz777 <76623656+achuzz777@users.noreply.github.com>
2022-05-01 08:19:50 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Abu_ser*:  ```" + msg + "```"
+    return "✅ *Xachuzz*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Abu_ser*:  ```" + msg + "```"
+    return "🛑 *Xachuzz*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Abu_ser*:  ```" + msg + "```"
+    return "⏺️ *Xachuzz*:  ```" + msg + "```"
 }
 
 
